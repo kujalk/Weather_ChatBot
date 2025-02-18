4b6cf5f4f6f8e0b5b790cf65c74ce278c15bbbbb
Janarthanan Kugathasan <jana19930918@gmail.com>
2025-02-18 22:17:20 +0800

    Updated the port for redirection

--- dockerfile
+++ dockerfile
@@ -6,8 +6,8 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY src/app_*.py app.py
+COPY src/app.py app.py
 
-EXPOSE 7860
+EXPOSE 80
 
 CMD ["python", "app.py"]
--- src/app.py
+++ src/app.py
@@ -307,4 +307,4 @@ def create_interface():
 
 if __name__ == "__main__":
     demo = create_interface()
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=80)
